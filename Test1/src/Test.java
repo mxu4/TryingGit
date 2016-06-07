7e6f26f7bd82ca36bbae54a9dccb539d4d9e89eb
Michael Xu <mxu4@gryphonscientific.com>
2016-06-07 10:43:33 -0400

    Editing in branch

--- Test1/src/Test.java
+++ Test1/src/Test.java
@@ -1,6 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Changed back to master branch editing");
+		System.out.println("Editing in a branch again");
 	}
 }
